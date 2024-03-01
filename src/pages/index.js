d71b8fb98f09153c2a770ed0f4fc7c81477b3aff
Yulduzxon Maxammatyunusova <yunusovayulduz85@gmail.com>
2024-03-01 22:23:53 +0500

    about us secton added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
+import AboutUs from "@/components/aboutUs";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}></main>
+    <main className={`${inter.className}`}>
+      <AboutUs />
+    </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        aboutTitle: "rgb(46, 58, 107)",
+        aboutBtnColor: "#c3242a"
+      }
     },
   },
   plugins: [],
