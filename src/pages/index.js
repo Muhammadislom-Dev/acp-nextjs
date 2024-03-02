eba4f9c1b70228e6d4cd2126bbd9f64f9424cb1f
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-03-02 23:59:52 +0500

    update contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Rating from "@/components/Rating";
 import ReplainWidget from "./replain";
 
 import { Unbounded } from "next/font/google";
+import Contact from "@/components/Contact";
 
 const unbounded = Unbounded({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
       <AboutUs />
       <Product />
       <Rating />
+      <Contact />
       <Footer />
       <ReplainWidget />
     </main>
