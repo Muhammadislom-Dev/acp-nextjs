aa4a97dde8c3b76fd25717f74031468e5949f010
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-03-04 01:37:31 +0500

    update fixed

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -44,11 +44,11 @@ function Language() {
 
       {isMenuOpen && (
         <div
-          className="origin-top-right absolute right-0 mt-2  rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          className="origin-top-right language absolute right-0 mt-2  rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="options-menu">
-          <div className="py-1" role="none">
+          <div className="py-1 language" role="none">
             <div
               onClick={() => onChangeLanguage("uz")}
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
