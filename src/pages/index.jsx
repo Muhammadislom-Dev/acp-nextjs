b707f45301d42cca5c706a7a295cbf2bce106737
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-03-06 00:08:20 +0500

    datagrid fixed bug

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Product from "@/components/Product";
-import AboutUs from "@/components/AboutUs";
 import Footer from "@/components/Footer";
 import Rating from "@/components/Rating";
 import ReplainWidget from "./replain";
@@ -10,6 +9,7 @@ import { Unbounded } from "next/font/google";
 import News from "@/components/News";
 import Contact from "@/components/Contact";
 import Partner from "@/components/Partner";
+import AboutUs from "@/components/AboutUs";
 
 
 const unbounded = Unbounded({ subsets: ["latin"] });
